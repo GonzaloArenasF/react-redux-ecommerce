05ea4778452a630efee98e3131100cddee0889e0
Gonzalo A. Arenas Flores <gonzalo.arenas@globant.com>
2020-02-05 09:44:08 -0300

    init dev

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <main>
+    <header></header>
+    <section></section>
+    <footer></footer>
+  </main>
+);
+
+export default App;
