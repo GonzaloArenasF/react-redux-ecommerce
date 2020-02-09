4979d2fd8c314439e4f301b2bed0c189a330ae77
Gonzalo A. Arenas Flores <gonzalo.arenas@globant.com>
2020-02-09 12:35:04 -0300

    Footer test ready

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
+// Component
 import FooterSection from './footer';
 
+const componentSetUp = (props = {}) => shallow(<FooterSection {...props} />);
+
 describe('Footer Section', () => {
-  test('Inner Texts', () => {
-    const { getByText } = render(<FooterSection />);
+  let component;
 
-    const topText = getByText(/Ecommerce developed over ReacJS and Redux/i);
-    expect(topText).toBeInTheDocument();
+  beforeEach(() => {
+    component = componentSetUp();
+  });
 
-    const bottomText = getByText(/Copyright 2020/i);
-    expect(bottomText).toBeInTheDocument();
+  test('Should render without errors', () => {
+    const wrapperFooter = component.find(`[data-test='footer']`)
+    expect(wrapperFooter.length).toBe(1);
+
+    const wrapperP = component.find('p');
+    expect(wrapperP.length).toBe(2);
+  });
+
+  test('Should appear the right messages', () => {
+    const wrapperFirstText = component.contains('Ecommerce developed over ReacJS and Redux');
+    expect(wrapperFirstText).toBeTruthy();
+
+    const wrapperSecondText = component.contains('Copyright 2020');
+    expect(wrapperSecondText).toBeTruthy();
   });
 });
